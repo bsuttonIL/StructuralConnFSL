3b60b502895884de986d8cd7c8ad5e27f5ca5ee8
psharp1289 <psharp1289@gmail.com>
2016-09-20 14:20:33 -0400

    fixed freesurfer pipeline and config logging steps

--- freesurfer_recon_all.sh
+++ freesurfer_recon_all.sh
@@ -7,6 +7,6 @@ sublist=$(<subjects.txt)
 for sub in $sublist; do
 	recon-all -i $T1_location/$sub/$T1_name -subjid "sub_${sub}"
 	recon-all -all -subjid "sub_${sub}"
-	echo "${sub} completed" >> freesurfer_completed.txt
+	echo "${sub} completed" >> $log_freesurfer_recon_all/freesurfer_completed.txt
 done
 
