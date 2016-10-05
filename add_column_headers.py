ff76eb0563cd1b4be9ebb1fa28482c9474e209af
Paul <psharp1289@gmail.com>
2016-10-05 14:33:15 -0400

    fixed add_headers to connectome python file to be flexible to multiple parcellaitons

--- add_column_headers.py
+++ add_column_headers.py
@@ -1,33 +1,30 @@
 
 import csv
 import os
+from ConfigParser import ConfigParser as ConfigParser
 
-Hagmann_Order = {}
-cortical_ROIS=[]
-with open('{}/fs_labels_hagmann_order.txt'.format(os.environ['CONN_DIR']), 'r') as f:
-	for line in range(68):
-		current_line = f.readline()
-		if line == 67:
-			current_line = current_line.split()
-		else:
-			current_line = current_line[:-1]
-			current_line = current_line.split()
-		Hagmann_Order[current_line[0]]=current_line[1]
-		cortical_ROIS.append(current_line[1])
+#get parcellation number from connectome config file
+get_config=CFP()
+parcellation_num=int(get_config.get('PARC_SCHEMES','parcellation_number'))
+pacellation_labels_file=get_config.get('PARC_SCHEMES','parcellation_labels_file')
+cortical_ROIS_list=[]
 
+#put info in labels file into a python list
+with open('{}/{}'.format(os.environ['CONN_DIR'],parcellation_labels_file), 'r') as f:
+	all_lines=f.readlines()
+	for lines in all_lines:
+		cortical_ROIS_list.append(lines.split()[1])
 
-
-with open('conn68_VolumeWeighted.csv', 'r') as f:
+#create nested list with ROI labels in first row to prepare to write to CSV file
+with open('conn{}_VolumeWeighted.csv'.format(parcellation_num), 'r') as f:
 	r = csv.reader(f)
 	newlines = [l for l in r]
 	newlines.insert(0,[])
-	newlines[0].append(0)
-	for i in range(68):
-		if i < 67:
-			newlines[0].append(0)
-		newlines[0][i] = Hagmann_Order[str(i+1)]
+	for i in range(parcellation_num):
+		newlines[0].append(cortical_ROIS_list[i])
 
-with open('conn68_VolumeWeighted_headers.csv', 'a') as f:
+#write labelled connectome with ROIs in first row to CSV file
+with open('conn{}_VolumeWeighted_headers.csv'.format(parcellation_num), 'a') as f:
 	w = csv.writer(f)
 	w.writerows(newlines)
 	
